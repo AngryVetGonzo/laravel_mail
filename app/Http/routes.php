2b0a0f13d1ce15a283908427553964eec72759c4
jesus <chuyelregio5@yahoo.com>
2017-09-27 20:15:01 -0700

    learning mailgun

---
+++ app/Http/routes.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+use Illuminate\Support\Facades\Mail;
+
+Route::get('/', function () {
+//    return view('welcome');
+
+    $data = [
+        'title' => 'HELLOOOO WORLD',
+        'content' => 'this is me learning laravel'
+    ];
+
+    Mail::send('emails.test', $data, function ($message) {
+        $message->to('chuyelregio5@yahoo.com', 'jesus')->subject('hello student hope this works');
+    });
+
+});
